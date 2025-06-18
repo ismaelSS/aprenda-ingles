09dd1e7ffcdb5eda3fa6b60fe0b0b3649ab78900
ismaelss <ismael1337sn@gmail.com>
2025-06-18 19:27:28 -0300

    add provisorious index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex flex-1 justify-center items-center bg-foreground">
-      <h1 className="text-3xl font-bold text-center">
-        aprenda inglês
-      </h1>
+    <div className="flex flex-col flex-1 justify-center items-center bg-foreground">
+      <p className="text-3xl font-bold text-center text-white tracking-[0.35rem]">
+        site em construção 
+      </p>
+
+      <p className="text-white">use o menu no canto superior esquerdo para navegar</p>
         
     </div>
   );
